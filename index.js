e6f748b5fbe658e4c63df0682aed6219a034b298
TeyPeyPey <teyonce143@gmail.com>
2025-02-21 03:12:27 -0600

    saving work

--- index.js
+++ index.js
@@ -93,54 +93,90 @@ const collection = [
 
 // PROBLEM #1 //
 
-const setDigitalCode = function(){
-   
+const setDigitalCode = function(array, value){
+for (var i = 0; i < array.length - 1; i++) {
+   array.digitalCode = value[i];
+}
+return array
 };
+console.log(setDigitalCode([1, 2, 3, 4], 2));
 
 // PROBLEM #2 //
 
-const filterByGenreTag = function(){
-    
+const filterByGenreTag = function(array, tag){
+//Create a function called filterByGenreTag that takes in two parameters - array and tag; 
+//array represents an array of movie objects; tag represents a string of a certain genre designation. 
+//This function should use the native filter method to return a new array of only the movie objects that contain the input genre tag.
+
 };
 
 
 
 // PROBLEM #3 //
 
-const filterBySpecialFeatureType = function(){
-    
+const filterBySpecialFeatureType = function(array, type){
+//Create a function called filterBySpecialFeatureType that takes in two parameters - array and type. 
+//array represents an array of movie objects; type represents a string describing the type of special feature. 
+//This function should use the native filter method to return a new array 
+//of only the movie objects that contain that type of special feature.
 };
 
 
 // PROBLEM #4 //
 
 const getTopSpecialFeature = function(array, title){
-    
+//Create a function called getTopSpecialFeature that takes in one parameter - array and title. 
+//array represents an array of movie objects; title represents a string of the title of a film. 
+//This function should use recursion to iterate through the array. 
+//If a movie with the title is found, the function should return a string of the movie's title followed by the title of the first special feature in the film's specialFeatures array. 
+//If no movie is found matching that title, the function should return "no movie found matching that title".
 };
 
 
 // PROBLEM #5 //
 
-const mapTitles = function(){
-    
+const mapTitles = function(array){
+//Create a function called mapTitles that takes in one parameter 
+//- array - which represents an array of movie objects. 
+//This function should use the native map method 
+//to return a new array of the movie's title, year, and director as strings.
+var output = [];
+return array.map('title', 'year', 'director');
 };
 
 
 
 // PROBLEM #6 //
 
-const mapSpecialFeatures = function(){
-    
+const mapSpecialFeatures = function(array){
+//Create a function called mapSpecialFeatures that takes in one parameter 
+//- array - which represents an array of movie objects. 
+//This function should use the native map method to return a new array subarrays. 
+//Each subarray in the array that is returned should be the strings of the titles of special features.
+var output = [];
+array.map = collection[i][5];
+return output.push(array);
 };
+console.log([1, 2, 3, 4]);
 
 // PROBLEM #7 //
 
-const createNonsenseString = function(){
-    
+const createNonsenseString = function(array, index){
+//Create a function called createNonsenseString that takes in two parameters - array and index. 
+//array represents an array of movie objects, and index represents the index of a string. 
+//This function should use the native reduce method to iterate over the array 
+//and accumulate a string by accessing the index of each movie's title and adding it to an output string.
+
 };
 
 // PROBLEM #8 //
 
-const getValues = function(){
-    
+const getValues = function(object, props){
+//Create a function called getValues that takes in two parameters - object and props. 
+//object represents an individual movie object 
+//and props represents an array of keys that exist in the object. 
+//This function should return a new array of the values that exist at those keys.
+var output = [];
+return output.push(object[key], object[key].props);
 };
+
